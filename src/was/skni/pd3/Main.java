f6a2e95fd752dab48f1fb69b3a2d6e1a1d457c0e
dnlwlnc <daniel.welenc@gmail.com>
2017-04-09 17:56:42 +0200

    Trzecia praca domowa.

--- src/was/skni/pd3/Main.java
+++ src/was/skni/pd3/Main.java
@@ -6,11 +6,12 @@ package was.skni.pd3;
 public class Main {
     public static void main(String[] args){
         Kwadrat nowyProstokat = new Kwadrat();
+        System.out.println(nowyProstokat);
         Kwadrat drugi = new Kwadrat(10,20);
-
-        System.out.println(drugi.poleProstokata());
+        int bokTestowy = 5;
+        System.out.println("Pole prostokąta to: " + drugi.poleProstokata());
         System.out.println(drugi.czyKwadrat());
-        System.out.println(drugi.ileKwadratow(2));
-        System.out.printf("%.2f", drugi.promienKola());
+        System.out.printf("W twój prostokąt można wpisać %d kwadratów o boku %d\n", drugi.ileKwadratow(bokTestowy), bokTestowy);
+        System.out.printf("Koło, które ma mieć pole równe polu podanego prostokąta musi mieć promień długości: %.2f", drugi.promienKola());
     }
 }
